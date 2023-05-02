e8270409c8d749006cf0de2d116e7608980d2e30
yrxns <819924033@qq.com>
2023-05-02 20:40:33 +0800

    ...

--- yecao/base/Timestamp.cc
+++ yecao/base/Timestamp.cc
@@ -1,4 +1,6 @@
 #include "Timestamp.h"
+#include <time.h>
+#include <sys/time.h>
 
 using namespace yecao;
 
@@ -6,5 +8,37 @@ static_assert(sizeof(Timestamp) == sizeof(int64_t), "Timestamp should be same si
 
 
 std::string Timestamp::toString() const {
+    char buf[32] = {0};
+    int64_t seconds = microSecondsSinceEpoch_ / kMicroSecondsPerSecond;
+    int64_t microseconds = microSecondsSinceEpoch_ % kMicroSecondsPerSecond;
+    std::snprintf(buf, sizeof(buf), "%" PRId64 ".%06" PRId64 "", seconds, microseconds);
+    return buf;
+}
 
+std::string Timestamp::toFormattedString(bool showMicroseconds) const {
+    char buf[64] = {0};
+    time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
+    struct tm tm_time;
+    gmtime_r(&seconds, &tm_time);
+
+    if (showMicroseconds) {
+        int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
+        std::snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d.%06d",
+                      tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+                      tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec,
+                      microseconds);
+    } else {
+        std::snprintf(buf, sizeof(buf), "%4d%02d%02d %02d:%02d:%02d",
+                      tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+                      tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+    }
+
+    return buf;
+}
+
+Timestamp Timestamp::now() {
+    struct timeval tv;
+    gettimeofday(&tv, nullptr);
+    int64_t seconds = tv.tv_sec;
+    return Timestamp(seconds * kMicroSecondsPerSecond + tv.tv_usec);
 }
